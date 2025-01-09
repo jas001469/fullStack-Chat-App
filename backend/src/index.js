a8f2dc4c71f12a24d423af43f71c13e8dcd53421
Jas Singh Ajmani <jas001469@gmail.com>
2025-01-09 17:18:45 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV==='production'){
   app.use(express.static(path.join(__dirname,'../frontend/dist')))
-  app.get('*',(req,res)=>res.sendFile(__dirname,'../','frontend','dist','index.html'))
+  app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'../','frontend','dist','index.html')))
 }else{
   app.get('/', (req, res) => {
     res.send({ message: 'Welcome to Chat App' })
